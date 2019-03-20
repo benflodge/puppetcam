685bcfcefd002f695b835f88a8f7edc9df2fad4a
Ben Frank Lodge <ben.lodge@headforwards.com>
2019-03-20 17:01:46 +0000

    Updated arguments in index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const puppeteer = require("./export");
 
 const argv = require("yargs")
-    .usage("Usage: $0 --exportname=test.webm' --url=[str] -w [str] -h [str]")
-    .example("$0 --exportname='test.webm' --url='http://tobiasahlin.com/spinkit/' -w 1280 -h 720")
+    .usage("Usage: $0 --exportname=[str]' --url=[str] --width=[int] --height=[int]")
+    .example("$0 --exportname='test.webm' --url='http://tobiasahlin.com/spinkit/' --width=1280 --height=720")
     .argv;
 
 function main() {
     const options = {
         url: argv.url || null,
         exportname: argv.exportname || null,
-        h: argv.h || null,
-        w: argv.w || null,
+        height: argv.height || null,
+        width: argv.width || null,
     }
     puppeteer.main(options)
 }
